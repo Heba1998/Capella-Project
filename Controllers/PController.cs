be6c115f05bbf63a5295cc45179dd649aebd2e03
unknown <hebaalmomani1998@gmail.com>
2022-02-07 17:06:37 +0200

    api

---
+++ Controllers/PController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    public class PController : Controller 
+    {
+        private readonly IDutchRepository _repository;
+
+        public PController(IDutchRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _repository.GetAllProducts();
+        }
+    }
+}
